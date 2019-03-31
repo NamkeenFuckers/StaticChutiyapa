7b1f87acdf0cbcca2bad0ccf6d436508ecb4c38e
Admin <Admin@Admin-PC>
2019-03-31 11:49:26 +0530

    first commit

---
+++ src/check/Checking.java
@@ -0,0 +1,34 @@
+package check;
+
+public class Checking {
+	
+	
+	
+	public String meth()
+	{
+		Sample s1 = new Sample();
+		System.out.println(s1.i);
+		System.out.println(Sample.j=90);
+		System.out.println(s1.hello());
+		System.out.println(s1.hi());
+		System.out.println(Sample.hi());
+//		System.out.println(Sample.hello());
+//		System.out.println(Sample.i);
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		
+		/*Sample s = new Sample();
+		System.out.println(s.i);
+		System.out.println(Sample.j);
+		System.out.println(s.hello());
+		System.out.println(Sample.hi());*/
+		Checking ch = new Checking();
+		ch.meth();
+		
+		
+		
+	}
+
+}
---
+++ src/check/Sample.java
@@ -0,0 +1,21 @@
+package check;
+
+public class Sample {
+
+	int i=50;
+	static int j =60;
+	
+	public String hello()
+	{
+		return("Non static Hello World");
+	}
+	
+	public static String hi()
+	{
+		return ("static Hi World");
+	}
+	
+	
+	
+
+}
